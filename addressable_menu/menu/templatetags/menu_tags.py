8d0b063b7a44a3b906dc5f9cf294fc7aa2542c1d
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-12-07 14:41:52 +0300

    fixed active menu item logic

--- addressable_menu/menu/templatetags/menu_tags.py
+++ addressable_menu/menu/templatetags/menu_tags.py
@@ -15,16 +15,28 @@ def draw_menu(context, menu_name):
     )
 
     menu_tree = {}
+    active_item = None
+    active_parents = []
+
+    for item in all_items:
+        if item.get_absolute_url() == request.path:
+            active_item = item
+            while active_item.parent is not None:
+                active_parents.append(active_item.parent)
+                active_item = active_item.parent
+
     for item in all_items:
         if item.parent is None:
             menu_tree[item] = []
 
-        if item.get_absolute_url() == request.path:
-            item.active = True
-        else:
-            item.active = False
+        item.active = item == active_item or item in active_parents
 
-        if item.parent:
-            menu_tree[item.parent].append(item)
+        if (
+            item == active_item
+            or item in active_parents
+            or (item.parent == active_item and active_item is not None)
+        ):
+            if item.parent:
+                menu_tree[item.parent].append(item)
 
     return {"menu_items": menu_tree}
